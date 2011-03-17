d9eb4678af1269948aca7464ee852571863758f4
David Arroyo <david.a.arroyo@gmail.com>
2011-03-17 19:54:19 -0400

    Removed remains of old pipe-based control loop

--- main.c
+++ main.c
@@ -6,20 +6,15 @@
 #include <fcntl.h>
 #include "apl.h"
 int quit;
+
 int main(void) {
-	int chan[2];
 	Biobuf *input;
 	quit = 0;
 	
-	if(pipe(chan)) {
-		fprint(2, "Pipe error\n");
+	if(!(input = Bfdopen(1, O_RDONLY))) {
+		fprint(2, "Cannot open input file\n");
 		exit(1);
-	} else {
-		fcntl(chan[0], F_SETFL, O_NONBLOCK);
-		fcntl(chan[1], F_SETFL, O_NONBLOCK);
 	}
-	input = Bfdopen(0, O_RDONLY);
-
 	while(!quit) {
 		print("\t");
 		parse(scan(input));
