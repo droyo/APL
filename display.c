d1306573d0ed8fda071a4e3783961dc4197628d4
David Arroyo <david.a.arroyo@gmail.com>
2011-03-21 08:39:11 -0400

    some code cleanup. Changed 'primitive' type to a flag on 'function'.

--- display.c
+++ display.c
@@ -1,38 +1,36 @@
 #include <utf.h>
 #include <fmt.h>
 #include <math.h>
+#include <stdlib.h>
 #include "apl.h"
 
+static void disp_number(array *);
+
 void disp(array *a) {
-	int i;
-	double *d;
 	switch(a->t) {
-	case assign:
-		print("←");
-		break;
-	case number: 
-		d = aval(a);
-		for(i = 0; i < a->n; i++) {
-			print(i?" ":"");
-			if(d[i] == INFINITY)
-				print("∞");
-			else if(d[i] == -INFINITY)
-				print("-∞");
-			else print("%g",d[i]);
-		}
-		break;
-	case string:
-		print("`%s'", (char*)aval(a));
-		break;
-	case empty: print("⍝"); break;
-	case null: print("∘"); break;
-	case primitive: case dydop: case monop:
-		if(a->n==1) {
-			print("%C", *(Rune*)a->m);
-			break;
-		}
-	default: print("%s", (char*)aval(a)); break;
-	case lparen: case rparen: case colon: break;
+	case assign:   print("←");       break;
+	case number:   disp_number(a);   break;
+	case empty:    print("⍝");       break;
+	case null:     print("∘");       break;
+	case lparen:case rparen:case colon: break;
+	case dydop:case monop:case function:
+		if(a->f&primitive)
+			print("%C", *(Rune*)aval(a));
+		else
+			print("%s", (char*)aval(a));break;
+	default: print("%s", (char*)aval(a));
 	}
 }
 
+static void disp_number(array *a) {
+	int i;
+	double *d = aval(a);
+	for(i = 0; i < a->n; i++) {
+		print(i?" ":"");
+		if(d[i] == INFINITY)
+			print("∞");
+		else if(d[i] == -INFINITY)
+			print("-∞");
+		else print("%g",d[i]);
+	}
+}
--- main.c
+++ main.c
@@ -7,28 +7,20 @@
 #include "apl.h"
 int quit;
 void *global_env;
+#define try(e,f,s) do{if(f){fprint(2,s"\n");goto e;}}while(0)
 
 int main(void) {
 	Biobuf *input;
 	array *result;
 	quit = 0;
 	
-	if(mem_init()) {
-		fprint(2, "Cannot init memory manager.\n");
-		exit(1);
-	}
-	if(const_init()) {
-		fprint(2, "Cannot init constants\n");
-		exit(1);
-	}
-	if(!(input = Bfdopen(0, O_RDONLY))) {
-		fprint(2, "Cannot open input file\n");
-		exit(1);
-	}
-	if(!(global_env = env_init())) {
-		fprint(2, "Cannot init environment\n");
-		exit(1);
-	}
+	try(a,mem_init(),"Can't init memory");
+	try(b,const_init(),"Can't init constants");
+	try(c,!(input=Bfdopen(0,O_RDONLY)),
+		"Cannot open input");
+	try(d,!(global_env=env_init()),
+		"Cannot init environment");
+
 	while(!quit) {
 		print("\t");
 		result = eval(global_env,scan(input));
@@ -38,9 +30,10 @@ int main(void) {
 		}
 	}
 	print("\nBye\n");
-	Bterm(input);
-	env_free(global_env);
-	const_free();
-	mem_free();
-	return 0;
+
+	  env_free(global_env);
+	d:Bterm(input);
+	c:const_free();
+	b:mem_free();
+	a:return 0;
 }
