8be1734ebfb4ddd27f625a0d742819d8744bb718
David Arroyo <david.a.arroyo@gmail.com>
2011-03-31 23:57:45 -0400

    Added indentation to printing of multi-line arrays. Not perfect, but better than what we had previously.

--- format.c
+++ format.c
@@ -20,23 +20,25 @@ static int Afmtb(Fmt*,array*);
 /* Numeric, String, and Boxed arrays */
 static int N   (Fmt*,double*,int);
 static int Nx1 (Fmt*,double*,int,int);
-static int NxM (Fmt*,double*,int,int,int);
-static int NxMx(Fmt*,double*,int,int*,int);
+static int NxM (Fmt*,int,double*,int,int,int);
+static int NxMx(Fmt*,int,double*,int,int*,int);
 
 static int Sx1 (Fmt*,Rune*,int);
-static int SxM (Fmt*,Rune*,int,int);
-static int SxMx(Fmt*,Rune*,int*,int);
+static int SxM (Fmt*,int,Rune*,int,int);
+static int SxMx(Fmt*,int,Rune*,int*,int);
 
-static int B   (Fmt*,array**);
-static int Bx1 (Fmt*,array**,int);
-static int BxM (Fmt*,array**,int,int);
-static int BxMx(Fmt*,array**,int*,int);
+static int B   (Fmt*,int,array**);
+static int Bx1 (Fmt*,int,array**,int);
+static int BxM (Fmt*,int,array**,int,int);
+static int BxMx(Fmt*,int,array**,int*,int);
 
 static int getw(array*);
+static int geti(Fmt*);
 static int llen(Rune*);
+static int pad(Fmt*,int);
 static int frame(Fmt*,int,Rune,Rune);
 static int any(Rune**,int);
-static int fmt_boxrow(Fmt*,Rune**,int*,int);
+static int fmt_boxrow(Fmt*,int,Rune**,int*,int);
 static Rune* rfind(Rune*, Rune);
 
 int fmt_init(void) {
@@ -64,6 +66,8 @@ static int Afmt(Fmt *f) {
 	case colon:  return fmtprint(f,":");
 	case lparen: return fmtprint(f,"(");
 	case rparen: return fmtprint(f,")");
+	case ldfns:  return fmtprint(f,"[");
+	case rdfns:  return fmtprint(f,"]");
 	case function:
 		if(a->f&primitive) 
 			return fmtrune(f,*(Rune*)aval(a));
@@ -74,30 +78,30 @@ static int Afmt(Fmt *f) {
 }
 
 static int Afmtn(Fmt *f,array *a) {
-	int *s = ashp(a), w = getw(a);
+	int *s = ashp(a), w = getw(a), i=geti(f);
 	switch(a->r) {
 	case 0:  return N(f,aval(a),0);
 	case 1:  return Nx1(f,aval(a),w,s[0]);
-	case 2:  return NxM(f,aval(a),w,s[0],s[1]);
-	default: return NxMx(f,aval(a),w,s,a->r);
+	case 2:  return NxM(f,i,aval(a),w,s[0],s[1]);
+	default: return NxMx(f,i,aval(a),w,s,a->r);
 	}
 }
 static int Afmts(Fmt *f,array *a) {
-	int *s = ashp(a);
+	int *s = ashp(a), i=geti(f);
 	switch(a->r) {
 	case 0:  return -1;
 	case 1:	 return Sx1(f,aval(a),s[0]);
-	case 2:  return SxM(f,aval(a),s[0],s[1]);
-	default: return SxMx(f,aval(a),s,a->r);
+	case 2:  return SxM(f,i,aval(a),s[0],s[1]);
+	default: return SxMx(f,i,aval(a),s,a->r);
 	}
 }
 static int Afmtb(Fmt *f,array *a) {
-	int *s = ashp(a);
+	int *s = ashp(a),i=geti(f);
 	switch(a->r) {
-	case 0:  return B(f,aval(a));
-	case 1:  return Bx1(f,aval(a),s[0]);
-	case 2:  return BxM(f,aval(a),s[0],s[1]);
-	default: return BxMx(f,aval(a),s,a->r);
+	case 0:  return B(f,i,aval(a));
+	case 1:  return Bx1(f,i,aval(a),s[0]);
+	case 2:  return BxM(f,i,aval(a),s[0],s[1]);
+	default: return BxMx(f,i,aval(a),s,a->r);
 	}
 }
 
@@ -106,22 +110,26 @@ static int N(Fmt *f, double *d, int w) {
 	if(*d ==-INFINITY) return fmtprint(f, "%*s", w,"-∞");
 	return fmtprint(f,"%*g", w, *d);
 }
-static int B(Fmt *f,array **a) {
+static int B(Fmt *f,int ind,array **a) {
 	Rune *s,*t,*l,*n; 
 	int w, e=-1;
 	if(!(s = runesmprint("%A",*a)))
 		return -1;
 	else { t = s;  w = llen(s);}
+	if(pad(f,ind))       goto Error;
 	if(frame(f,w,UL,UR)) goto Error;
 
 	while(*t) {
 		if(fmtrune(f,'\n')) goto Error;
 		l = rfind(t,'\n');
 		n = *l? l + 1 : l; *l = 0;
+		if(pad(f,ind))
+			goto Error;
 		if(fmtprint(f,"%C%S%C",VE,t,VE))
 			goto Error;
 		t = n;
 	}
+	if(pad(f,ind))       goto Error;
 	if(fmtrune(f,'\n'))  goto Error;
 	if(frame(f,w,DL,DR)) goto Error;
 	e = 0;
@@ -137,7 +145,7 @@ static int Nx1(Fmt *f, double *d, int w, int n) {
 	return 0;
 }
 
-static int Bx1(Fmt *f, array **a, int n) {
+static int Bx1(Fmt *f, int ind, array **a, int n) {
 	Rune **u,**t; int i,k,*w,e = -1;
 	if(!(w = malloc(sizeof *w * n))) return -1;
 	if(!(u = malloc(sizeof *u * n))) goto Error1;
@@ -148,8 +156,10 @@ static int Bx1(Fmt *f, array **a, int n) {
 		else { t[k] = u[k]; w[k] = llen(u[k]); }
 	}
 	for(i=0;i<n;i++)if (frame(f,w[i],UL,UR)) goto Error3;
-	while(any(t,n)) if (fmt_boxrow(f,t,w,n)) goto Error3;
+	while(any(t,n)) 
+		if (fmt_boxrow(f,ind,t,w,n))         goto Error3;
 	if(fmtrune(f,'\n'))                      goto Error3;
+	if(pad(f,ind))                           goto Error3;
 	for(i=0;i<n;i++)if (frame(f,w[i],DL,DR)) goto Error3;
 	e = 0;
 	Error3: while(--k>=0) free(u[k]);
@@ -164,17 +174,19 @@ static int Sx1(Fmt *f, Rune *s, int n) {
 	return 0;
 }
 
-static int NxM(Fmt *f, double *d, int w, int m, int n) {
+static int NxM(Fmt *f, int ind, double *d, int w, int m, int n) {
 	int i; for(i=0;i<m;i++) {
-		if(Nx1(f, d+i*n,w,n)) return -1;
+		if(i&&pad(f,ind))    return -1;
+		if(Nx1(f,d+i*n,w,n)) return -1;
 		if(fmtstrcpy(f,i<m?"\n":""))
 			return -1;
 	}
 	return 0;
 }
 
-static int SxM(Fmt *f, Rune *s, int m, int n) {
+static int SxM(Fmt *f,int ind, Rune *s, int m, int n) {
 	int i; for(i=0; i<m; i++) {
+		if(i&&pad(f,ind))   return -1;
 		if(Sx1(f, s+i*n,n)) return -1;
 		if(fmtstrcpy(f,i<m?"\n":""))
 			return -1;
@@ -182,21 +194,21 @@ static int SxM(Fmt *f, Rune *s, int m, int n) {
 	return 0;
 }
 
-static int BxM(Fmt *f, array **a, int m, int n) {
+static int BxM(Fmt *f,int ind, array **a, int m, int n) {
 	int i; for(i=0;i<m;i++) {
-		if(Bx1(f, a+i*n,n)) return -1;
+		if(Bx1(f,ind,a+i*n,n)) return -1;
 		if(fmtstrcpy(f,i<m?"\n":""))
 			return -1;
 	}
 	return 0;
 }
 
-static int NxMx(Fmt *f,double *d, int w, int *s, int r) {
+static int NxMx(Fmt *f,int ind,double *d, int w, int *s, int r) {
 	int i, o;
-	if(r == 2) return NxM(f,d,w,s[0],s[1]);
+	if(r == 2) return NxM(f,ind,d,w,s[0],s[1]);
 	for(i=o=1;i<r;i++) o *= s[i];
 	for(i=0;i<s[0];i++) {
-		if(NxMx(f,d+(o*i),w,s+1,r-1)) 
+		if(NxMx(f,ind,d+(o*i),w,s+1,r-1)) 
 			return -1;
 		else if(fmtstrcpy(f,i<s[0]-1?"\n":"")) 
 			return -1;
@@ -204,12 +216,12 @@ static int NxMx(Fmt *f,double *d, int w, int *s, int r) {
 	return 0;
 }
 
-static int BxMx(Fmt *f, array **a, int *s, int r) {
+static int BxMx(Fmt *f,int ind, array **a, int *s, int r) {
 	int i, o;
-	if(r == 2) return BxM(f,a,s[0],s[1]);
+	if(r == 2) return BxM(f,ind,a,s[0],s[1]);
 	for(i=o=1;i<r;i++) o *= s[i];
 	for(i=0;i<s[0];i++) {
-		if(BxMx(f,a+(o*i),s+1,r-1))
+		if(BxMx(f,ind,a+(o*i),s+1,r-1))
 			return -1;
 		else if(fmtstrcpy(f,i<s[0]-1?"\n":""))
 			return -1;
@@ -217,12 +229,12 @@ static int BxMx(Fmt *f, array **a, int *s, int r) {
 	return 0;
 }
 
-static int SxMx(Fmt *f, Rune *s, int *sh, int r) {
+static int SxMx(Fmt *f,int ind, Rune *s, int *sh, int r) {
 	int i, o;
-	if(r == 2) return SxM(f,s,sh[0],sh[1]);
+	if(r == 2) return SxM(f,ind,s,sh[0],sh[1]);
 	for(i=o=1;i<r;i++)o *= sh[i];
 	for(i=0;i<sh[0];i++) {
-		if(SxMx(f,s+(o*i),sh+1,r-1))
+		if(SxMx(f,ind,s+(o*i),sh+1,r-1))
 			return -1;
 		else if(fmtstrcpy(f,i<sh[0]-1?"\n":""))
 			return -1;
@@ -237,6 +249,15 @@ static int getw(array *a) {
 		m=m<(n=snprint(buf,8,"%g",d[i]))?n:m;
 	return m;
 }
+static int geti(Fmt *f) {
+	int i = 0;
+	char *c = f->to;
+	while(c --> (char*)f->start) {
+		if(*c == '\n') break;
+		i++;
+	}
+	return i;
+}
 static Rune* rfind(Rune *s, Rune r) {
 	int i; for(i=0;s[i];i++)
 		if(s[i] == r) break;
@@ -244,6 +265,9 @@ static Rune* rfind(Rune *s, Rune r) {
 }
 static int llen(Rune *s) {
 	return rfind(s,'\n') - s - 1;
+}
+static int pad(Fmt *f, int x) {
+	return fmtprint(f,"%*c",x+1,0);
 }
 static int frame(Fmt *f, int n, Rune b, Rune e) {
 	int i;
@@ -255,12 +279,13 @@ static int any(Rune **r, int n) {
 	int i; for(i=0;i<n;i++) if(*r[i]) return 1;
 	return 0;
 }
-static int fmt_boxrow(Fmt *f, Rune **blocks, int *w, int n) {
+static int fmt_boxrow(Fmt *f,int ind,Rune **blocks,int *w,int n) {
 	int i; Rune *s, *e;
 	for(i=0;i<n;i++) {
 		s = blocks[i];
 		e = rfind(s,'\n');
 		if(!i && fmtrune(f,'\n')) return -1;
+		if(!i && pad(f,ind))      return -1;
 		if(*e) { blocks[i] = e+1; *e = 0; }
 		else blocks[i] = e;
 		if(fmtprint(f,"%C%*S%C",VE,w[i]+1,s,VE)) return -1;
